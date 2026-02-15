29ec809fc2f92a4c07acb5428520ff5d8ce6898f
Abhishek Mathur <abhishekmathur200624@gmail.com>
2026-02-15 14:21:03 +0530

    feat: initialize counter app with React and Vite
    - Add package.json with dependencies and scripts for development
    - Create SVG assets for UI components (cross, info, minus, plus, reload, settings, vite)
    - Implement main application structure in App.jsx with state management for counter
    - Style application with CSS for layout and design
    - Configure Vite for React application

---
+++ counter/src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [reset, setReset] = useState(false);
+  return (
+    <>
+      <nav>
+        <button style={{ backgroundColor: "black", border: "none" }}>
+          <img src="/images/info.svg" alt="" height={24} />
+        </button>
+        <button style={{ backgroundColor: "black", border: "none" }}>
+          <img src="/images/settings.svg" alt="" height={24} />
+        </button>
+        <button
+          style={{ backgroundColor: "black", border: "none" }}
+          onClick={() => {
+            setReset(true);
+          }}
+        >
+          <img src="/images/reload.svg" alt="" height={24} />
+        </button>
+        {/* <img src="/images/reload.svg" alt="" height={24}/> */}
+      </nav>
+      <div className="main">
+        <button
+          className="min"
+          onClick={() => {
+            setCount(count - 1);
+          }}
+        >
+          <img src="/images/minus.svg" alt="" height={54} />
+        </button>
+        <div className="number min">
+          <h1>{count}</h1>
+        </div>
+        <button
+          className="min max"
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          <img src="/images/plus.svg" alt="" height={54} />
+        </button>
+      </div>
+      {reset && (
+        <div className="reset">
+          <img
+            onClick={() => {
+              setReset(false);
+            }}
+            src="/images/cross.svg"
+            style={{ color: "white" }}
+            alt=""
+            height={34}
+          />
+          <h2>Reset counter?</h2>
+          <button onClick={()=> {
+            setCount(0)
+            setReset(false)
+          }}>Yes</button>
+          <button
+            onClick={() => {
+              setReset(false);
+            }}
+          >
+            No
+          </button>
+        </div>
+      )}
+      <div className="setting">
+          
+      </div>
+    </>
+  );
+}
+
+export default App;
