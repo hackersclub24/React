704ef4aafbff76a4ce995aeedeee1a68810b7b9b
Abhishek Mathur <abhishekmathur200624@gmail.com>
2026-03-19 00:31:32 +0530

    Add TypeScript configuration file for Next.js project

---
+++ todos-nextjs/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="  flex justify-center items-center h-screen">
+        <div>
+          <h1 className="text-2xl font-bold">Add your todos</h1>
+        </div>
+      </div>
+    </>
+  );
+}
